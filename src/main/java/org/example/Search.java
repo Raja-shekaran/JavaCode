ac4c132bdb3fd99424b1b65832ca84731b9e1741
Raja-shekaran <raja1999.rsrs@gmail.com>
2025-03-16 21:31:56 +0530

    16-Mar-2025

--- src/main/java/org/example/Search.java
+++ src/main/java/org/example/Search.java
@@ -9,6 +9,7 @@ public class Search {
         System.out.println(linearSearch(arr, target));
         Arrays.sort(arr);
         System.out.println(binarySearch(arr, target));
+        System.out.println(binarySearchRecursive(arr, target, 0 , arr.length-1));
     }
 
     public static int linearSearch(int arr[], int target) {
@@ -34,6 +35,19 @@ public class Search {
                 right = mid - 1;
             }
 
+        }
+        return -1;
+    }
+
+    public static int binarySearchRecursive(int[] arr, int target, int left, int right) {
+        int mid = left + (right - left) / 2;
+        if (target == arr[mid])
+            return mid;
+        if (target > arr[mid]) {
+            binarySearchRecursive(arr, target, mid + 1, right);
+        }
+        else{
+            binarySearchRecursive(arr, target, left, mid - 1);
         }
         return -1;
     }
