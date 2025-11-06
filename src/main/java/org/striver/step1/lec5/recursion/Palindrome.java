9e551ef44c28c4200b27d6be02bce40051668392
Raja-shekaran <raja1999.rsrs@gmail.com>
2025-11-06 23:53:46 +0530

    06Nov - CheckIn

---
+++ src/main/java/org/striver/step1/lec5/recursion/Palindrome.java
@@ -0,0 +1,16 @@
+package org.striver.step1.lec5.recursion;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(palindrome(0,"mom"));
+    }
+
+    public static boolean palindrome(int i, String s)
+    {
+        if(i>=s.length()/2) return true;
+
+        if(s.charAt(i)!=s.charAt(s.length()-i-1)) return false;
+
+        return palindrome(i+1,s);
+    }
+}
---
+++ src/main/java/org/striver/step1/lec5/recursion/SumOfN.java
@@ -0,0 +1,24 @@
+package org.striver.step1.lec5.recursion;
+
+import java.util.Scanner;
+
+public class SumOfN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(sum(N,0));
+        System.out.println(sumFunction(N));
+    }
+
+    //1. Parameterized way
+    public static int sum(int N, int sum){
+        if(N==0) return sum;
+        return sum(N-1,sum+N);
+    }
+
+    //2. Functional way
+    public static int sumFunction(int N){
+        if(N==0) return 0;
+        return N + sumFunction(N-1);
+    }
+}
