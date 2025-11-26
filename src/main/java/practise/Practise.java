ce2f9f50f395b66a89160d7ec38561dd8c22d13d
Raja-shekaran <raja1999.rsrs@gmail.com>
2025-11-27 00:02:41 +0530

    26Nov - CheckIn

--- src/main/java/practise/Practise.java
+++ src/main/java/practise/Practise.java
@@ -5,32 +5,26 @@ import java.util.Map;
 
 public class Practise {
     public static void main(String[] args) {
-        int[] arr = new int[]{3, 2, 3, 2, 2, 3, 3, 4};
+        int[] arr = new int[]{4, 5, 1, 2, 0, 4};
         Map<Integer, Integer> hashMap = new LinkedHashMap<>();
+
         for (int num : arr) {
             hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
         }
-        for(Map.Entry<Integer,Integer> entry: hashMap.entrySet()){
-            if(entry.getValue()>1){
+
+        boolean found = false;
+
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            if (entry.getValue() == 1) {
                 System.out.println(entry.getKey());
+                found = true;
                 break;
             }
         }
-        //Boyer–Moore
-//        int candidate = 0;
-//        int count = 0;
-//
-//        for(int num: arr){
-//            if(count == 0)
-//            {
-//                candidate = num;
-//            }
-//            if(num == candidate){
-//                count++;
-//            }else {
-//                count--;
-//            }
-//        }
-//        System.out.println(candidate);
+
+        if (!found) {
+            System.out.println(-1);
+        }
+
     }
 }
