48583bb0357a62e10730095e1f3840e4ba5a96ea
Raja-shekaran <raja1999.rsrs@gmail.com>
2025-11-28 23:39:35 +0530

    28Nov - CheckIn

--- src/main/java/practise/Practise.java
+++ src/main/java/practise/Practise.java
@@ -4,22 +4,65 @@ import java.util.Arrays;
 
 public class Practise {
     public static void main(String[] args) {
-        int[] arr = new int[]{5,2,4,6,1};
-        insertionSort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = { 64, 25, 12, 22, 11 };
+
+        System.out.print("Original array: ");
+        printArray(arr);
+
+        mergeSort(arr,0,arr.length-1);
+
+        System.out.print("Sorted array: ");
+        printArray(arr);
     }
 
-    public static void insertionSort(int[] arr){
-        for(int i=1;i<arr.length;i++)
-        {
-            int key = arr[i];
-            int j = i - 1;
-            while(j>=0 && arr[j]>key)
-            {
-                arr[j+1]=arr[j];
-                j--;
-            }
-            arr[j+1]=key;
-        }
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low == high) return;
+
+        int mid = ( low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        merge(arr, low, mid, high);
     }
+
+    public static void merge(int[] arr, int low, int mid, int high)
+    {
+        int left = low;
+        int right = mid+1;
+        int index=0;
+        int[] temp = new int[high - low + 1];
+        // Merge until one side is exhausted
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[index++] = arr[left++];
+            } else {
+                temp[index++] = arr[right++];
+            }
+        }
+
+        // Leftover left half
+        while (left <= mid) {
+            temp[index++] = arr[left++];
+        }
+
+        // Leftover right half
+        while (right <= high) {
+            temp[index++] = arr[right++];
+        }
+
+        // Copy back to original array
+        for (int i = 0; i < temp.length; i++) {
+            arr[low + i] = temp[i];
+        }
+
+    }
+
+    public static void printArray(int[] arr){
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
 }
