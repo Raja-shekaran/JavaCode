130793f539e40d01ee74e4273cd995b1c20bae59
Raja-shekaran <raja1999.rsrs@gmail.com>
2025-12-06 19:29:27 +0530

    6Dec - CheckIn

---
+++ src/main/java/practise/Practise3.java
@@ -0,0 +1,10 @@
+package practise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Practise3 {
+    public static void main(String[] args) {
+        String input = "banana";
+    }
+}
