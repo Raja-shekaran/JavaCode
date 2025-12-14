1078de68945fb98ff493a9ceada48d813d0c42d7
Raja-shekaran <raja1999.rsrs@gmail.com>
2025-12-14 19:56:11 +0530

    14Dec - CheckIn

---
+++ src/main/java/org/striver/step3/lec1/NumberAppearOnce.java
@@ -0,0 +1,12 @@
+package org.striver.step3.lec1;
+
+public class NumberAppearOnce {
+    public static void main(String[] args) {
+        int[] arr = {2,2,1};
+        int xor = 0;
+        for(int num : arr){
+            xor ^= num;
+        }
+        System.out.println(xor);
+    }
+}
