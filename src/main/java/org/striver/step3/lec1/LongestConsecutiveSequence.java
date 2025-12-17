327c95e63e06f2d70bbfc253477e449820ddd805
Raja-shekaran <raja1999.rsrs@gmail.com>
2025-12-17 23:22:11 +0530

    17Dec - CheckIn

---
+++ src/main/java/org/striver/step3/lec1/LongestConsecutiveSequence.java
@@ -0,0 +1,22 @@
+package org.striver.step3.lec1;
+
+import java.util.Arrays;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        int[] arr = {102,4,100,1,101,3,2,1,1};
+        Arrays.sort(arr);
+        int longest = 1 ,lastSmaller = Integer.MIN_VALUE, countCurrent = 0;
+        for (int j : arr) {
+            if (j - 1 == lastSmaller) {
+                countCurrent++;
+                lastSmaller = j;
+            } else if (j != lastSmaller) {
+                countCurrent = 1;
+                lastSmaller = j;
+            }
+            longest = Math.max(longest, countCurrent);
+        }
+        System.out.println(longest);
+    }
+}
